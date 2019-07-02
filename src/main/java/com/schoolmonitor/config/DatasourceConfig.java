dec3aed5240a9c2118a18f653c3d51acd3a787b5
yash <28.yashverma@gmail.com>
2019-07-02 09:26:29 +0530

    Working code for multiple databases + h2

--- src/main/java/com/schoolmonitor/config/DatasourceConfig.java
+++ src/main/java/com/schoolmonitor/config/DatasourceConfig.java
@@ -1,46 +1,42 @@
-//package com.schoolmonitor.config;
-//
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Import;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-//
-//import com.schoolmonitor.config.*;
-//import com.zaxxer.hikari.HikariDataSource;
-//
-//@Configuration
+package com.schoolmonitor.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
 //@Import(SecurityConfig.class)
-//public class DatasourceConfig {
-//
-//	
-//
-//	@Bean
-//	@Primary
-//	@ConfigurationProperties("app.datasource.schoolmonitor")
-//	public DataSourceProperties firstDataSourceProperties() {
-//		return new DataSourceProperties();
-//	}
-//
-//	@Bean
-//	@Primary
-//	@ConfigurationProperties("app.datasource.schoolmonitor.configuration")
-//	public HikariDataSource firstDataSource() {
-//		return firstDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
-//	}
-//
-//	@Bean
-//	@ConfigurationProperties("app.datasource.school")
-//	public DataSourceProperties secondDataSourceProperties() {
-//		return new DataSourceProperties();
-//	}
-//
-//	@Bean
-//	@ConfigurationProperties("app.datasource.school.configuration")
-//	public HikariDataSource secondDataSource() {
-//		return secondDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
-//	}
-//
-//}
+public class DatasourceConfig {
+
+	@Bean
+	@Primary
+	@ConfigurationProperties("spring.datasource.schoolmonitor")
+	public DataSourceProperties firstDataSourceProperties() {
+		return new DataSourceProperties();
+	}
+
+	@Bean
+	@Primary
+	@ConfigurationProperties("spring.datasource.schoolmonitor.configuration")
+	public HikariDataSource firstDataSource() {
+		return firstDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
+	}
+
+	@Bean
+	@ConfigurationProperties("spring.datasource.school")
+	public DataSourceProperties secondDataSourceProperties() {
+		return new DataSourceProperties();
+	}
+
+	@Bean
+	@ConfigurationProperties("spring.datasource.configuration")
+	public HikariDataSource secondDataSource() {
+		return secondDataSourceProperties().initializeDataSourceBuilder().type(HikariDataSource.class).build();
+	}
+
+}
