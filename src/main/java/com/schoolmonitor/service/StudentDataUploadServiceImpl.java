f6bcc46fe47f4c0e4300b5f30d92589f42b42e86
Prabhjeet Singh <prabhjeet.singh92@outlook.com>
2020-01-09 23:39:20 +0530

    latest state

--- src/main/java/com/schoolmonitor/service/StudentDataUploadServiceImpl.java
+++ src/main/java/com/schoolmonitor/service/StudentDataUploadServiceImpl.java
@@ -27,34 +27,10 @@ public class StudentDataUploadServiceImpl implements StudentDataUploadService {
 	SubscriptionRepository subscriptionRepository;
 	@Autowired
 	CredentialsRepository credentialsRepository;
-	private static final String FILE_DIRECTORY = "/var/files";
+	
 	@Override
 	public Void studentDataUpload(MultipartFile studentDataFile) throws IOException {
-		//Path filePath = Paths.get(/*FILE_DIRECTORY + "/" +*/ studentDataFile.getOriginalFilename());
-		//Files.copy(studentDataFile.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
-		
-
-		/*if(credentialsRepository.findByUserName(principal.getName()).getIsAdmin()==1) {
-		int subscriptionId;
-		String loggedInDomain = TenantContext.getCurrentTenant();
-		School school = schoolRepository.findByDomainForLogin(loggedInDomain);
-		if (null != school) {
-			subscriptionId = school.getSubscriptionId();
-			Subscription subscription = subscriptionRepository.findById(subscriptionId).get();
-			if (subscription.getSubscriptionStatus().equalsIgnoreCase("NEW")) {
-				subscription.setSubscriptionStatus("CONTINUED");
-				File loggedInAdminFolder=new File("/schoolmonitor-backend/src/main/resources"+loggedInDomain+"_"+principal.getName());
-				if(!loggedInAdminFolder.exists()) {
-					loggedInAdminFolder.mkdir();
-					File fileToUpload=new File();
-					}
-			} else {
-
-			}
-		}
-		}
-
-*/		return null;
+				return null;
 	}
 
 }
